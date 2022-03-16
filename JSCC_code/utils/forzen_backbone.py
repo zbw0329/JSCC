3985c8a8ba7a80ed943a7ceea20a370cc4abcd45
zbw0329 <50667397+zbw0329@users.noreply.github.com>
2022-03-16 08:34:08 +0800

    Delete JSCC_code directory

--- JSCC_code/utils/forzen_backbone.py
+++
@@ -1,6 +0,0 @@
-def forzen_backbone(x):
-    for name, value in x.named_parameters():
-        if 'layer' in name:
-            value.requires_grad = False
-    print(f"Backbone's parameters have been frozen")
-    return x
